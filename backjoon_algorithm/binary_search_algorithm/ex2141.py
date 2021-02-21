e183712444fd856a88a9fefb7aafd130e3b46406
dbswl4951 <dbswl31024@naver.com>
2021-02-22 03:40:43 +0900

    2021_02_21

---
+++ backjoon_algorithm/binary_search_algorithm/ex2141.py
@@ -0,0 +1,21 @@
+#우체국
+import sys
+
+n=int(sys.stdin.readline().strip())
+town=[]
+for _ in range(n):
+    town.append(list(map(int,sys.stdin.readline().split())))
+town.sort(key=lambda x:x[0])
+s=0
+for t in town:
+    s+=t[1]
+#평균 값 구하기
+s=(s+1)//2
+s2=0
+#왼쪽->오른쪽으로 이동하면서 확인
+for t in town:
+    s2+=t[1]
+    #왼쪽편 사람들과 오른쪽편 사람들의 인구수가 비슷해지는 곳의 위치 구함
+    if s2>=s:
+        print(t[0])
+        break
--- backjoon_algorithm/greedy_algorithm/test.py
+++ backjoon_algorithm/greedy_algorithm/test.py
@@ -1,27 +1,14 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-def dfs(x, y):
-    if dp[x][y]: return dp[x][y]
-    dp[x][y] = 1
-    print("x,y=======",x,y)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n:
-            if s[x][y] < s[nx][ny]:
-                print("nx,ny:",nx,ny)
-                dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
-                print("dp[",x,"] [",y,"] :::",dp[x][y])
-            print("dp====",dp)
-    return dp[x][y]
-n = int(input())
-s = [list(map(int, input().split())) for i in range(n)]
-dp = [[0] * n for i in range(n)]
-result = 0
+n, k = map(int, input().split())
+s = list(input().strip())
+t, tk = [], k
+print(s)
 for i in range(n):
-    for j in range(n):
-        result = max(result, dfs(i, j))
-print(result)
+    print("s[i]===",s[i])
+    while tk > 0 and t and t[-1] < s[i]:
+        print("tk,t::",tk,t)
+        del t[-1]
+        tk -= 1
+    t.append(s[i])
+print(''.join(t[:n - k]))
