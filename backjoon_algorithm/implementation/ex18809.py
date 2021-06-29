8abc4d881a6bc541164c7add09ead709d0977857
dbswl4951 <dbswl31024@naver.com>
2021-06-30 00:29:14 +0900

    2021_06_29

--- backjoon_algorithm/implementation/ex18809.py
+++ backjoon_algorithm/implementation/ex18809.py
@@ -11,43 +11,98 @@ from collections import deque
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-def bfs(board,visited,q):
+def bfs(board,case,green):
+    count=0
+    greenQ,redQ=deque(),deque()
+
+    # 초록색, 빨간색 배양액 큐에 넣기
+    for x,y in case:
+        if [x,y] in green: greenQ.append([x,y])
+        else: redQ.append([x,y])
+
+    while greenQ:
+        # 한 턴마다의 결과 저장
+        greenSet,redSet=set(),set()
+        while greenQ:
+            x,y=greenQ.popleft()
+            board[x][y]=3
+            for i in range(4):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m:
+                    if board[nx][ny]==1 or board[nx][ny]==2: greenSet.add((nx,ny))
+        while redQ:
+            x,y=redQ.popleft()
+            board[x][y]=4
+            for i in range(4):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m:
+                    if board[nx][ny]==1 or board[nx][ny]==2: redSet.add((nx,ny))
+
+        inter=greenSet&redSet
+        greenSet=greenSet-inter
+        redSet=redSet-inter
+        # 교집합은 배양액끼리 만난 곳 => 꽃 피우기
+        for x,y in inter:
+            count+=1
+            board[x][y]=5
+        for x,y in greenSet: board[x][y]=3
+        for x,y in redSet: board[x][y]=4
+
+        greenQ.extend(greenSet)
+        redQ.extend(redSet)
+    return count
+
+n,m,g,r=map(int,sys.stdin.readline().split())
+board=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+able=[]
+for i in range(n):
+    for j in range(m):
+        if board[i][j]==2: able.append([i,j])
+
+result=0
+for case in combinations(able,g+r):
+    # 초록색 배양액 선택
+    for green in combinations(case,g):
+        copyBoard=copy.deepcopy(board)
+        result=max(result,bfs(copyBoard,case,green))
+print(result)
+
+
+# 1차 시도 => 시간 초과
+'''
+import sys,copy
+from itertools import combinations
+from collections import deque
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def bfs(board,q,visited):
     global result
     count=0
-
     while q:
         qLen=len(q)
-        red=set()
-        green=set()
+        # 배양액이 합쳐 질 수 있는 곳 저장
+        visitSet = set()
+        
         while qLen:
-            x, y = q.popleft()
-            print("x,y:",x,y)
-            # 이미 꽃 피운 자리는 넘어가기
-            if not board[x][y]: continue
-            color=board[x][y]
-
+            x,y=q.popleft()
+            if board[x][y]==0: qLen-=1; continue
             for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if 0<=nx<n and 0<=ny<m and board[nx][ny] and not visited[nx][ny]:
-                    print("nx,ny:",nx,ny)
-                    if (color==3 and (nx,ny) in red) or (color==4 and (nx,ny) in green):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m and board[nx][ny]!=0 and not visited[nx][ny]:
+                    if (nx,ny) in visitSet and board[nx][ny]!=board[x][y]:
                         count+=1
                         board[nx][ny]=0
-                        print("c:",count)
-                        print("board:",board)
                     else:
+                        visitSet.add((nx,ny))
                         board[nx][ny]=board[x][y]
                         q.append([nx,ny])
-                        print("board:",board)
-                        print("q:",q)
-                        if board[nx][ny]==3: green.add((nx,ny));print("green:",green)
-                        else: red.add((nx,ny));print("red:",red)
-            qLen-=1
-            for gx,gy in green:
-                if not visited[gx][gy]: visited[gx][gy] = 1
-            for rx,ry in red:
-                if not visited[rx][ry]: visited[rx][ry] = 1
-    print("count====",count)
+            qLen -= 1
+
+        for vx,vy in visitSet:
+            if board[vx][vy]!=0:
+                visited[vx][vy]=1
     result=max(result,count)
 
 n,m,g,r=map(int,sys.stdin.readline().split())
@@ -59,10 +114,8 @@ for i in range(n):
 
 result=0
 for case in combinations(able,g+r):
-    print("case:",case)
     # 초록색 배양액 선택
     for select in combinations(case,g):
-        print("select:",select)
         q=deque()
         copyBoard=copy.deepcopy(board)
         visited = [[0] * m for _ in range(n)]
@@ -76,6 +129,6 @@ for case in combinations(able,g+r):
                 copyBoard[c[0]][c[1]]=4
                 q.append([c[0], c[1]])
                 visited[c[0]][c[1]]=1
-        print(copyBoard)
-        bfs(copyBoard,visited,q)
+        bfs(copyBoard,q,visited)
 print(result)
+'''
