cddcbb8ec438e3361d3f13cc7b986d56cb9db27a
dbswl4951 <dbswl31024@naver.com>
2021-07-12 20:30:25 +0900

    2021_07_12

--- backjoon_algorithm/prefix_sum/ex17425.py
+++ backjoon_algorithm/prefix_sum/ex17425.py
@@ -1,4 +1,11 @@
 #약수의 합
+'''
+dp[i] : i보다 작거나 같은 자연수의 모든 약수의 합
+
+ex)
+dp[4] = (1)+(1+2)+(1+3)+(1+2+4) = 15
+dp[5] = (1)+(1+2)+(1+3)+(1+2+4)+(1+5) = dp[4]+(5의 약수의 합) = 21
+'''
 import sys
 
 t=int(sys.stdin.readline().strip())
@@ -6,9 +13,9 @@ arr=[int(sys.stdin.readline().strip()) for _ in range(t)]
 dp=[0]*1000001
 # 에라토스테네스의 체
 for i in range(1,1000001):
-    # i의 약수의 합을 더함
+    # j의 약수의 합을 더함
     for j in range(i,1000001,i):
         dp[j]+=i
-    # i보다 작은 자연수의 약수 합을 더함
+    # i전까지의 자연수의 약수 합을 더함
     dp[i]+=dp[i-1]
 for a in arr: print(dp[a])
